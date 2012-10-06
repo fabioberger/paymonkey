52487b3a24524f15ad298bf193bb89c23975e074
Soroush Pour <soroush@wealthlift.com>
2012-10-06 03:25:18 -0400

    Added new db tables to cleardb.php, minor localhost config changes

--- cleardb.php
+++ cleardb.php
@@ -55,31 +55,61 @@ if ($db) {
 $CREATETABLES = false;
 
 if ($CREATETABLES) {
-$query = 'CREATE TABLE `users` (
-  `userid` int(14) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(150) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `avatar` varchar(150) NOT NULL,
-  PRIMARY KEY (`userid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;';
 
-$result = mysql_query($query);
-if (!$result) {
-	echo mysql_error() . "\n";
-}
+	$query = 'CREATE TABLE `users` (
+	  `userid` int(14) NOT NULL AUTO_INCREMENT,
+	  `name` varchar(100) NOT NULL,
+	  `email` varchar(100) NOT NULL,
+	  `password` varchar(150) NOT NULL,
+	  `username` varchar(100) NOT NULL,
+	  `avatar` varchar(150) NOT NULL,
+	  PRIMARY KEY (`userid`)
+	) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;';
 
-$query = 'CREATE TABLE `fbconnect_users` (
-  `id` int(14) NOT NULL AUTO_INCREMENT,
+	$result = mysql_query($query);
+	if (!$result) {
+		echo mysql_error() . "\n";
+	}
+
+	$query = 'CREATE TABLE `fbconnect_users` (
+	  `id` int(14) NOT NULL AUTO_INCREMENT,
+	  `userid` int(14) NOT NULL,
+	  `connectid` int(50) NOT NULL,
+	  PRIMARY KEY (`id`)
+	) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;';
+
+	$result = mysql_query($query);
+	if (!$result) {
+		echo mysql_error() . "\n";
+	}
+
+	$query = 'CREATE TABLE `groups` (
+	  `groupid` int(14) NOT NULL AUTO_INCREMENT,
+	  `lead_userid` int(14) NOT NULL,
+	  `total_amt` int(10) NOT NULL,
+	  `num_payers` int(2) NOT NULL DEFAULT '0',
+	  PRIMARY KEY (`groupid`)
+	) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;'
+
+	$result = mysql_query($query);
+	if (!$result) {
+		echo mysql_error() . "\n";
+	}
+
+	$query = 'CREATE TABLE `payments` (
+  `paymentid` int(14) NOT NULL AUTO_INCREMENT,
   `userid` int(14) NOT NULL,
-  `connectid` int(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;';
+  `groupid` int(14) NOT NULL,
+  `paid` tinyint(1) NOT NULL DEFAULT '0',
+  `paid_date` datetime NOT NULL,
+  `pay_amt` int(10) NOT NULL,
+  PRIMARY KEY (`paymentid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;'
 
-$result = mysql_query($query);
-if (!$result) {
-	echo mysql_error() . "\n";
-}
+	$result = mysql_query($query);
+	if (!$result) {
+		echo mysql_error() . "\n";
+	}
+
 }
 ?>
