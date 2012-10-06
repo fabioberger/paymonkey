95bda70eb8179d42e95b915dab4aab45a5288577
Andrew Gerst <gerst20051@gmail.com>
2012-10-06 00:17:36 -0400

    'db'

--- cleardb.php
+++ cleardb.php
@@ -56,14 +56,14 @@ $CREATETABLES = true;
 
 if ($CREATETABLES) {
 $query = 'CREATE TABLE `users` (
-  `userid` int(14) NOT NULL AUTO_INCREMENT,"
+  `userid` int(14) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(150) NOT NULL,
   `username` varchar(100) NOT NULL,
   `avatar` varchar(150) NOT NULL,
   PRIMARY KEY (`userid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;';
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;';
 
 $result = mysql_query($query);
 if (!$result) {
@@ -75,7 +75,7 @@ $query = 'CREATE TABLE `fbconnect_users` (
   `userid` int(14) NOT NULL,
   `connectid` int(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ';
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;';
 
 $result = mysql_query($query);
 if (!$result) {
