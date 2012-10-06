7bb89d73d58d2643ecf84ca84612eaa2c81ce869
fabioberger <fabioberger1991@gmail.com>
2012-10-05 23:21:02 -0400

    added db schemes

--- cleardb.php
+++ cleardb.php
@@ -56,5 +56,24 @@ $CREATETABLES = false;
 
 if ($CREATETABLES) {
 
+CREATE TABLE `users` (
+  `userid` int(14) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `avatar` varchar(150) NOT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
+
+CREATE TABLE `fbconnect_users` (
+  `id` int(14) NOT NULL AUTO_INCREMENT,
+  `userid` int(14) NOT NULL,
+  `connectid` int(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
 }
 ?>
