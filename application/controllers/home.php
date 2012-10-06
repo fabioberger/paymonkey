272adb4b67e19537c2a39a4c7214b6712f810e58
Andrew Gerst <gerst20051@gmail.com>
2012-10-06 02:16:00 -0400

    updates home

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,40 +1,23 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Home extends MY_Controller {
-
 	public function index(){
-
 		$this->title = "PayMonkey";
+		$this->css[] = "home.css";
 		$this->_render('pages/homepage');
-
 	}
-	
-	public function reference(){	
-		
-		/*
-		 *set up title and keywords (if not the default in custom.php config file will be set) 
-		 */
+
+	public function reference(){
 		$this->title = "Yaaaaa";
 		$this->keywords = "arny, arnodo";
-		
 		$this->_render('pages/home');
 	}
 
 	public function pick_friends() {
-
 		$this->load->library('facebook');
-
 		$facebook = New Facebook();
-
 		$friends = $facebook->getFriends();
-
 		$this->data['friends'] = $friends;
-
 		$this->_render('pages/pick_friends');
-
-
 	}
-
-
-	
 }
--- application/views/pages/homepage.php
+++ application/views/pages/homepage.php
@@ -4,8 +4,27 @@
 <h1>PayMonkey</h1>
 </div>
 <div class="row">
-<div class="span4">
-<div class="facebook_connect"><a href="<?=base_url(); ?>facebook_connect/login_redirect"><img src="<?php echo base_url(); ?>resources/images/facebookconnect2.png" width="200px" height="35px"></a></div>
+<div class="span12">
+<img id="image_paymonkey" src="http://sweetclipart.com/multisite/sweetclipart/files/monkey_with_banana.png">
+</div>
+</div>
+<div class="row">
+<div class="span12">
+<div id="homepage_header">paymonkey</div>
+</div>
+</div>
+<div class="row">
+<div class="span12">
+<div id="homepage_subtitle">no more monkey business</div>
+</div>
+</div>
+<div class="row">
+<div class="span12">
+<div class="facebook_connect">
+<a href="<?=base_url(); ?>facebook_connect/login_redirect">
+<button id="b_fbconnect" class="btn btn-success" type="button"><div id="fbconnect_text">Connect with Facebook</div></button>
+</a>
+</div>
 </div>
 </div>
 </section>
