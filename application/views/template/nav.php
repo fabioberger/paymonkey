86ac96bb48be3caf38674fd143de6ee4c10130d7
Andrew Gerst <gerst20051@gmail.com>
2012-10-06 02:44:19 -0400

    reduced homepage

--- application/views/template/nav.php
+++ application/views/template/nav.php
@@ -1,8 +1,7 @@
 <ul class="nav">
+<?php
+/*
 <li class="active"><a class="<?php echo isActive($pageName,"home")?>" href="<?php echo base_url()?>">Home</a></li>
-<li><a class="<?php echo isActive($pageName,"about")?>" href="<?php echo base_url()?>">Link</a></li>
-<li><a class="<?php echo isActive($pageName,"about")?>" href="<?php echo base_url()?>">Link</a></li>
-<li><a class="<?php echo isActive($pageName,"about")?>" href="<?php echo base_url()?>">Link</a></li>
-<li><a class="<?php echo isActive($pageName,"about")?>" href="<?php echo base_url()?>">Link</a></li>
-<li><a class="<?php echo isActive($pageName,"about")?>" href="<?php echo base_url()?>">Link</a></li>
+*/
+?>
 </ul>
