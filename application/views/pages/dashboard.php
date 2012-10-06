728c1eb65c26e7c29c7a7b0917d70b89518ac8b1
fabioberger <fabioberger1991@gmail.com>
2012-10-06 04:06:08 -0400

    friend finder

--- application/views/pages/dashboard.php
+++ application/views/pages/dashboard.php
@@ -6,11 +6,38 @@
 <div class="row">
 <div class="span9">
 
-<?php print_r($friends); ?>
+<script type="text/css">
+	
+	.member {
+		width:200px;
+		height:100px;
+		float:left;
+		padding:5px;
+	}
+
+	.friend_img {
+		width:180px;
+		height:85px;
+	}
+
+	.friend_name {
+		width:200px;
+		height:15px;
+		font-weight:bold;
+	}
+
+</script>
+
 
 <?php 
 	foreach($friends as $friend_id => $friend_name) {
-	?><div class="member"><img src="http://graph.facebook.com/<?=$friend_id; ?>/picture" /></div><?php
+	?>
+	<div class="member">
+		<div class="friend_img">
+			<img src="http://graph.facebook.com/<?=$friend_id; ?>/picture" />
+		</div>
+		<div class="friend_name"><?=$friend_name; ?></div>
+	</div><?php
 	}
 ?>
 
