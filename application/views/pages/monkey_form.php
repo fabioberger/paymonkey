1a5cca2d818366a76ee62bbad7cd2b43f1a3b54a
Andrew Gerst <gerst20051@gmail.com>
2012-10-06 03:53:37 -0400

    mform

--- application/views/pages/monkey_form.php
+++ application/views/pages/monkey_form.php
@@ -3,23 +3,22 @@
 <div id="monkeyform_content">
 <form class="form-horizontal">
 <div class="control-group">
-<label class="control-label" for="inputEmail">Email</label>
+<label class="control-label" for="inputEmail">Paypal Email</label>
 <div class="controls">
-<input type="text" id="inputEmail" placeholder="Email">
+<input type="text" id="inputEmail" placeholder="Paypal Email">
 </div>
 </div>
 <div class="control-group">
 <label class="control-label" for="inputPassword">Password</label>
 <div class="controls">
-<input type="password" id="inputPassword" placeholder="Password">
+<div class="input-prepend">
+<span class="add-on">@</span><input class="span2" id="prependedInput" size="16" type="text" placeholder="Username">
+</div>
 </div>
 </div>
 <div class="control-group">
 <div class="controls">
-<label class="checkbox">
-<input type="checkbox"> Remember me
-</label>
-<button type="submit" class="btn">Sign in</button>
+<button type="submit" class="btn btn-primary">Create Monkey</button>
 </div>
 </div>
 </form>
