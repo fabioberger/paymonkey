a572af03fd23931f71f87806428dbc6415961373
Andrew Gerst <gerst20051@gmail.com>
2012-10-05 23:27:01 -0400

    db

--- cleardb.php
+++ cleardb.php
@@ -52,28 +52,29 @@ if ($db) {
 	}
 }
 
-$CREATETABLES = false;
+$CREATETABLES = true;
 
 if ($CREATETABLES) {
 
-CREATE TABLE `users` (
-  `userid` int(14) NOT NULL AUTO_INCREMENT,
+$query = 'CREATE TABLE `users` (".
+  `userid` int(14) NOT NULL AUTO_INCREMENT,"
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(150) NOT NULL,
   `username` varchar(100) NOT NULL,
   `avatar` varchar(150) NOT NULL,
   PRIMARY KEY (`userid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;';
 
+mysql_query($query);
 
-
-CREATE TABLE `fbconnect_users` (
+$query = 'CREATE TABLE `fbconnect_users` (
   `id` int(14) NOT NULL AUTO_INCREMENT,
   `userid` int(14) NOT NULL,
   `connectid` int(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ';
 
+mysql_query($query);
 }
 ?>
