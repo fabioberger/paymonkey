0006491d83a4d041279c2aeb731c6d1be0778114
fabioberger <fabioberger1991@gmail.com>
2012-10-10 15:25:19 -0400

    add locations

---
+++ application/controllers/location.php
@@ -0,0 +1,25 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Location extends MY_Controller {
+	
+	public function friend_locations(){
+		$this->title = "Locations";
+		
+		$this->load->model('friend_model');
+		$userid = $this->session->userdata['userid'];
+		$allfriends = $this->input->post('allfriends');
+		$temp_friends = explode(",", $allfriends);
+		$friends = array();
+		for ($i=0; $i < count($temp_friends); $i=$i+2) { 
+			if ($temp_friends[$i] != '') {
+				$fbid = $temp_friends[$i];
+				$name = $temp_friends[($i+1)];
+				$friends[$fbid] = $name;
+			}
+		}
+		
+		$this->_render('pages/locations');
+	}
+
+	
+}
