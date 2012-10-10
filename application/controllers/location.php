7dd3ae9224b74ca247718b80471a950dd540c33d
fabioberger <fabioberger1991@gmail.com>
2012-10-10 16:21:24 -0400

    add google maps

--- application/controllers/location.php
+++ application/controllers/location.php
@@ -18,13 +18,31 @@ class Location extends MY_Controller {
 			}
 		}
 
+		$this->load->library('GMap');
+
+		$this->gmap->GoogleMapAPI();
+
+		// valid types are hybrid, satellite, terrain, map
+		$this->gmap->setMapType('hybrid');
+
 		$this->load->library('facebook');
 		$facebook = New Facebook();
 		$friend_infos = array();
 		foreach($friends as $friend_id => $name) {
 			$friend_info = $facebook->getFriendInfo($friend_id);
 			$friend_infos[$friend_id] = $friend_info;
+			if(array_key_exists('location', $friend_info)) {
+			$this->gmap->addMarkerByAddress($friend_info['location']['name'], $friend_info['name'], "Visit here!");
+			}
 		}
+
+		$this->data['headerjs'] = $this->gmap->getHeaderJS();
+		$this->data['headermap'] = $this->gmap->getMapJS();
+		$this->data['onload'] = $this->gmap->printOnLoad();
+		$this->data['map'] = $this->gmap->printMap();
+		$this->data['sidebar'] = $this->gmap->printSidebar();
+
+		/* END google maps */
 		
 		$this->data['friend_infos'] = $friend_infos;
 		$this->_render('pages/locations');
--- application/views/pages/locations.php
+++ application/views/pages/locations.php
@@ -13,6 +13,13 @@
 		}
 		?>
 	</div>
+
+	<?php echo $headerjs; ?>
+	<?php echo $headermap; ?>
+
+	<?php echo $onload; ?>
+	<?php echo $map; ?>
+	<?php echo $sidebar; ?>
 </div>
 </section>
 </div>
