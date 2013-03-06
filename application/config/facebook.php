d529780b62bcac198d32877594592c2c2b8a377f
fabioberger <fabioberger1991@gmail.com>
2013-03-06 00:19:50 -0500

    Remove retroactively sensitive info

---
+++ application/config/facebook.php
@@ -0,0 +1,6 @@
+<?php
+	
+	$config['fb_appid' ]     = "YOUR_APP_ID_HERE";
+	$config['fb_secret']     = "YOUR_SECRET_HERE";
+	$config['fb_baseurl']    = "http://YOURDOMAIN.com/facebook_connect/connect/"; //return url from requests
+
